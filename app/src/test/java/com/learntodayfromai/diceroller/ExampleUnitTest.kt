d684734ef5b2cbb8c1871cede031acc32583ce79
Learn Today <learntodayfromai@gmail.com>
2023-04-13 14:43:11 -0700

    added unit tests

--- app/src/test/java/com/learntodayfromai/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/learntodayfromai/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.learntodayfromai.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun genNumber(){
+        val dice = Dice(6)
+        val roll = dice.roll()
+        assertTrue("value between 1 and 6",roll in (1..6))
     }
 }
